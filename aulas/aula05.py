7d4b952c1af62d794587c7bcb29a730fd4ec6797
Davy Caetano <davy.caetano08@gmail.com>
2026-08-15 11:52:55 -0300

    fim da aula

--- aulas/aula05.py
+++ aulas/aula05.py
@@ -1,20 +1,29 @@
 frase = 'Curso em Vídeo Python'
 
 '''
-print(frase[9:21:2])
-print(frase[:5])
-print(frase[15:])
-print(frase[9::3])
+print(frase[3])
+print(frase[1:15])
+print(frase[1:15:2])
+print(frase[:13])
+print(frase[13:])
+print(frase[::2])
 '''
 
 '''
+print("""Welcome! Are you completely new to programming?
+about why and how to get started with Python. Fortunately
+an expericed programmer in any programming language
+(whatever it may be) can pick Python very quickly.
+It's also easy for beginners to use and learn, so jump in""")
+'''
+
 print(frase)
-print(len(frase))
+print(len(frase.strip()))
 print(frase.count('o', 0, 13))
 print(frase.find('deo'))
 print(frase.find('Android'))
 print('Curso' in frase)
-'''
+
 '''
 print(frase.replace('Python', 'Android'))
 print(frase.upper())
@@ -31,7 +40,3 @@ print(frase.strip())
 print(frase.rstrip())
 print(frase.lstrip())
 '''
-
-#print(frase.split())
-frase = frase.split()
-print('-'.join(frase))
