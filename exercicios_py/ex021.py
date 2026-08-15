49c13f8a5f4cc2957cbf36fa5bf0f40f27797d53
Davy Caetano <davy.caetano08@gmail.com>
2026-08-15 11:07:21 -0300

    ajuste de código

--- exercicios_py/ex021.py
+++ exercicios_py/ex021.py
@@ -1,8 +1,15 @@
 import random
+import time
 
-alunos = ['lucas', 'Gabriel', 'Helena', 'Rafael',
-           'Bianca', 'Leonardo']
-limite_lista = 6
-ordem = random.sample(alunos, limite_lista)
+alunos = []
+limite_lista = int(input('Quantos alunos tem?: '))
 
-print(f'A ordem de apresentação é: {ordem}')
+while len(alunos) in range(limite_lista):
+    nome = input('Qual o nome do aluno?: ')
+    alunos.append(nome.capitalize())
+
+print(f'A lista não embaralhada é: {alunos}')
+random.shuffle(alunos)
+time.sleep(2)
+
+print(f'A ordem de apresentação é: {alunos}')
