143dafe45bd99402508876ed6f66ee1e5f6a6104
Davy <davy.caetano08@gmail.com>
2026-08-17 16:16:44 -0300

    Final adjustments class 10

--- exercicios_py/ex008.py
+++ exercicios_py/ex008.py
@@ -1,8 +1,8 @@
 lista_notas = []
 limite_notas = int(input('Defina uma quantidade de notas: '))
 
-for i in range(limite_notas):
-    i = float(input(f'Digite sua nota: '))
+for i in range(1, limite_notas + 1):
+    i = float(input(f'Digite sua {i}º nota: '))
     lista_notas.append(i)
 
 avg = sum(lista_notas) / len(lista_notas)
