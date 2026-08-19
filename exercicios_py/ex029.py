e85ba0bd1c3d732da53faf09ddd44924d9473620
Davy Caetano <davy.caetano08@gmail.com>
2026-08-19 12:45:59 -0300

    ajuste de código

--- exercicios_py/ex029.py
+++ exercicios_py/ex029.py
@@ -1,6 +1,9 @@
-import random
+from random import randint
 
-num = random.randint(1, 5)
+num = randint(1, 5) #computador escolhe um número aleatório
+
+print(('-=-' * 15),'\nVou escolher um número aleatório')
+print('-=-' * 15)
 
 while True:
     escol_user = int(input('Escolha um número entre 1 e 5:\n'))
