9cd1375a1b7b9544bc305b2c5481c96e2f28c0bd
Davy Caetano <davy.caetano08@gmail.com>
2026-08-25 11:44:38 -0300

    ajuste de código

--- exercicios_py/ex046.py
+++ exercicios_py/ex046.py
@@ -1,28 +1,27 @@
 from random import choice
 
 valores = ('Pedra', 'Papel', 'Tesoura')
-vez_pc = choice(valores)
+computador = choice(valores)
+jogador = input('Escolha uma jogada:\n Pedra, Papel ou Tesoura: ').capitalize()
 
-vez_jog = input('Escolha uma jogada:\n Pedra, Papel ou Tesoura: ').capitalize()
-
-if vez_pc == valores[0] and vez_jog == valores[1]:
-    print(f'{vez_pc}!\nParabéns, você ganhou!')
-elif vez_pc == valores[0] and vez_jog == valores[2]:
-    print(f'{vez_jog}!\nEu ganhei! ha ha ha')
-elif vez_pc == vez_jog:
-    print(f'{vez_pc}!\nQue pena, empatamos!')
+if computador == valores[0] and jogador == valores[1]:
+    print(f'{computador}!\nParabéns, você ganhou!')
+elif computador == valores[0] and jogador == valores[2]:
+    print(f'{jogador}!\nEu ganhei! ha ha ha')
+elif computador == jogador:
+    print(f'{computador}!\nQue pena, empatamos!')
 else:
-    if vez_pc == valores[1] and vez_jog == valores[0]:
-        print(f'{vez_pc}!\nEu ganhei! ha ha ha')
-    elif vez_pc == valores[1] and vez_jog == valores[2]:
-        print(f'{vez_pc}!\nParabéns, você ganhou!')
-    elif vez_pc == vez_jog:
-        print(f'{vez_pc}!\nQue pena, empatamos!')
+    if computador == valores[1] and jogador == valores[0]:
+        print(f'{computador}!\nEu ganhei! ha ha ha')
+    elif computador == valores[1] and jogador == valores[2]:
+        print(f'{computador}!\nParabéns, você ganhou!')
+    elif computador == jogador:
+        print(f'{computador}!\nQue pena, empatamos!')
     else:
-        if vez_pc == valores[2] and vez_jog == valores[1]:
-            print(f'{vez_pc}!\nEu ganhei! ha ha ha')
-        elif vez_pc == valores[2] and vez_jog == valores[0]:
-            print(f'{vez_pc}!\nParabéns, você ganhou!')
+        if computador == valores[2] and jogador == valores[1]:
+            print(f'{computador}!\nEu ganhei! ha ha ha')
+        elif computador == valores[2] and jogador == valores[0]:
+            print(f'{computador}!\nParabéns, você ganhou!')
         else:
-            print(f'{vez_pc}!\nQue pena, empatamos!')
+            print(f'{computador}!\nQue pena, empatamos!')
             
