b96d01be9080841fe7fc2ae02b2b81ac722f51d6
Davy Caetano <davy.caetano08@gmail.com>
2026-08-25 11:12:36 -0300

    revisão exercícios aula 12

--- exercicios_py/ex044.py
+++ exercicios_py/ex044.py
@@ -1,7 +1,8 @@
 peso_kg = float(input('Digite seu peso em KG: '))
 altura_m = float(input('Digite sua altura em METROS: '))
 imc = peso_kg / altura_m ** 2
-def get_imc(imc: float):
+
+def classifica_imc(imc: float): #retorna classificação
     if imc < 18.5:
         classificacao ='abaixo do peso'
     elif imc <= 25:
@@ -17,4 +18,4 @@ def get_imc(imc: float):
 if peso_kg == 0 or altura_m == 0:
     print('Peso ou altura inválido!')
 else:
-    print(f'Seu IMC é {imc:.2f} e se enquadra como: {get_imc(imc)}')
+    print(f'Seu IMC é {imc:.2f} e se enquadra como: {classifica_imc(imc)}')
