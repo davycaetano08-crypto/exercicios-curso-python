29e635c5ffb9e31ce3f628b44b58205e6df537be
Davy Caetano <davy.caetano08@gmail.com>
2026-08-25 22:34:09 -0300

    Fim da aula 13 (corrigindo exercícios)

--- exercicios_py/ex047.py
+++ exercicios_py/ex047.py
@@ -4,6 +4,9 @@ print(f'{' Estouro de fogos ':=^30}')
 sleep(0.5)
 
 for i in range(10, 0, -1):
-    print(f'Os estouros acontecem em {i}')
+    if i >= 10:
+        print(f'Os estouros acontecem em {i}')
+    else:
+        print(i)
     sleep(1)
 print('Estouros!!!!!')
--- exercicios_py/ex057.py
+++ exercicios_py/ex057.py
@@ -1,7 +1,18 @@
-nome
+cadastro = []
 
-for i in range(1, 5):
-    nome = input('Digite seu nome: ')
+for i in range(3):
+    nome = input('Digite seu nome: ').capitalize()
     idade = int(input('Digite sua idade: '))
-    secho = input('Digite seu sexo: ')
+    sexo = input('Digite seu sexo: ').capitalize()
 
+    dados_pessoa = {
+        "name": nome,
+        "age": idade,
+        'sex': sexo
+    }
+
+    cadastro.append(dados_pessoa)
+    print('Pessoa Cadastrada!')
+
+for c in cadastro:
+    print({c['name']}, {c['age']}, {c['sex']})
